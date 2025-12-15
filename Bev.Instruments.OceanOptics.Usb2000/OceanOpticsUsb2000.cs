7ae2058426ccf4bf11c2893bcb89c66618f8602e
Michael Matus <michael@matus.at>
2025-12-15 23:00:51 +0100

    Add project files.

---
+++ Bev.Instruments.OceanOptics.Usb2000/OceanOpticsUsb2000.cs
@@ -0,0 +1,78 @@
+﻿using Bev.Instruments.ArraySpectrometer.Abstractions;
+using System;
+
+namespace Bev.Instruments.OceanOptics.Usb2000
+{
+    public partial class OceanOpticsUsb2000 : IArraySpectrometer
+    {
+        private int nPixels;
+        private readonly int specIndex;
+        private double[] wavelengthsCache;
+        private double integrationTimeSeconds;
+
+        public OceanOpticsUsb2000() : this(0) { } // for now, hardcode to use first enumerated spectrometer
+
+        public OceanOpticsUsb2000(int specIndex)
+        {
+            this.specIndex = specIndex;
+            if(!OpenAndInitialize())
+                throw new Exception("Failed to open and initialize Ocean Optics USB2000 spectrometer.");
+            SetIntegrationTime(MinimumIntegrationTime);
+        }
+
+        public string InstrumentManufacturer => "Ocean Optics";
+        public string InstrumentType => GetSpectrometerType();
+        public string InstrumentSerialNumber => GetSerialNumber();
+        public string InstrumentFirmwareVersion => GetVersion();
+        public double[] Wavelengths => wavelengthsCache;
+        public double MinimumWavelength => wavelengthsCache[0];
+        public double MaximumWavelength => wavelengthsCache[wavelengthsCache.Length - 1];
+        public double SaturationLevel => 4000;
+        public double MinimumIntegrationTime => 0.003;
+        public double MaximumIntegrationTime => 65.0;
+
+        public double GetIntegrationTime() => integrationTimeSeconds; // there is no SeaBreeze call to read it back
+
+        public double[] GetIntensityData()
+        {
+            double[] result = null;
+            mutex.WaitOne();
+            try
+            {
+                int error = 0;
+                double[] spec = new double[nPixels];
+                SeaBreezeWrapper.seabreeze_get_formatted_spectrum(specIndex, ref error, ref spec[0], nPixels);
+                if (checkSeaBreezeError("get_formatted_spectrum", error))
+                {
+                    // KLUDGE: Some spectrometers (e.g. HR4000) insert non-pixel data 
+                    // into the first few pixels of the spectrum they report, which
+                    // we can override here to avoid confusing EDC and stray noise on
+                    // graphs.
+                    // 
+                    // TODO: Put in appropriate logic based on spectrometer model.
+                    for (int i = 0; i < 5; i++)
+                        spec[i] = spec[5];
+                    result = spec;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error getting spectrum: {e}");
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+            return result;
+        }
+
+        public void SetIntegrationTime(double seconds)
+        {
+            if (seconds < MinimumIntegrationTime) seconds = MinimumIntegrationTime;
+            if (seconds > MaximumIntegrationTime) seconds = MaximumIntegrationTime;
+            integrationTimeSeconds = seconds;
+            SetIntegrationTimeMilliseconds(seconds * 1000);
+        }
+
+    }
+}
---
+++ Bev.Instruments.OceanOptics.Usb2000/OceanOpticsUsb2000priv.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace Bev.Instruments.OceanOptics.Usb2000
+{
+    public partial class OceanOpticsUsb2000
+    {
+        static Mutex mutex = new Mutex();
+
+        private string GetSpectrometerType()
+        {
+            string result = string.Empty;
+            mutex.WaitOne();
+            try
+            {
+                byte[] slot = new byte[SeaBreezeWrapper.SLOT_LENGTH];
+                int error = 0;
+                SeaBreezeWrapper.seabreeze_get_model(specIndex, ref error, ref slot[0], SeaBreezeWrapper.SLOT_LENGTH);
+                if (checkSeaBreezeError("get_spectrometer_type", error))
+                    result = ByteToString(slot);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error getting spectrometer type: {e}");
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+            return result;
+        }
+
+        private string GetSerialNumber()
+        {
+            string result = string.Empty;
+            mutex.WaitOne();
+            try
+            {
+                byte[] slot = new byte[SeaBreezeWrapper.SLOT_LENGTH];
+                int error = 0;
+                SeaBreezeWrapper.seabreeze_get_serial_number(specIndex, ref error, ref slot[0], SeaBreezeWrapper.SLOT_LENGTH);
+                if (checkSeaBreezeError("get_serial_number", error))
+                    result = ByteToString(slot);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error getting serial number: {e.Message}");
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+            return result;
+        }
+
+        private string GetVersion()
+        {
+            string result = string.Empty;
+            mutex.WaitOne();
+            const int MAX_VERSION_LEN = 80;
+            try
+            {
+                byte[] version = new byte[MAX_VERSION_LEN];
+                int error = 0;
+                SeaBreezeWrapper.seabreeze_get_api_version_string(ref version[0], version.Length);
+                if (checkSeaBreezeError("get_api_version_string", error))
+                    result = ByteToString(version);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error getting version string: {e.Message}");
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+            return result;
+        }
+
+        private bool SetIntegrationTimeMilliseconds(double ms)
+        {
+            bool result = false;
+            mutex.WaitOne();
+            try
+            {
+                int error = 0;
+                SeaBreezeWrapper.seabreeze_set_integration_time_microsec(specIndex, ref error, (long)(ms * 1000));
+                result = checkSeaBreezeError("set_integration_time_microsec", error);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error setting integration time: {e}");
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+            return result;
+        }
+
+        private bool OpenAndInitialize()
+        {
+            wavelengthsCache = null;
+            int error = 0;
+            SeaBreezeWrapper.seabreeze_open_spectrometer(specIndex, ref error);
+            if (!checkSeaBreezeError("open_spectrometer", error))
+                return false;
+            nPixels = SeaBreezeWrapper.seabreeze_get_formatted_spectrum_length(specIndex, ref error);
+            if (!checkSeaBreezeError("get_formatted_spectrum_length", error))
+                return false;
+            double[] tmp = new double[nPixels];
+            SeaBreezeWrapper.seabreeze_get_wavelengths(specIndex, ref error, ref tmp[0], nPixels);
+            if (!checkSeaBreezeError("get_wavelengths", error))
+                return false;
+            wavelengthsCache = tmp;
+            return true;
+        }
+
+        // returns true if last operation was successful, false if last operation had an error
+        private bool checkSeaBreezeError(string operation, int errorCode)
+        {
+            if (errorCode == SeaBreezeWrapper.ERROR_SUCCESS)
+                return true;
+            byte[] buffer = new byte[64];
+            SeaBreezeWrapper.seabreeze_get_error_string(errorCode, ref buffer[0], 64);
+            string msg = ByteToString(buffer);
+            Console.WriteLine($"[SeaBreeze] error during {operation}: {msg}");
+            return false;
+        }
+
+        private string ByteToString(byte[] buf)
+        {
+            int len = 0;
+            while (buf[len] != 0 && len + 1 < buf.Length)
+                len++;
+            byte[] clean = Encoding.Convert(Encoding.GetEncoding("iso-8859-1"), Encoding.UTF8, buf);
+            return Encoding.UTF8.GetString(clean, 0, len);
+        }
+
+
+    }
+}
